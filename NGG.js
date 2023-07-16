ce1b5ac7c0caf6e269cd4da3a19098aa37f7ea90
OG-09 <136126650+OG-09@users.noreply.github.com>
2023-07-16 21:55:13 +0100

    Add files via upload

---
+++ NGG.js
@@ -0,0 +1,59 @@
+let ans = genrandnum();
+//gendrandnum = generate random number
+let attempts = 0;
+
+//function to generate random number
+function genrandnum() {
+    return Math.floor(Math.random()*100)+1;
+}
+
+//function to test user number and display results
+function confirm() {
+    var usernum = parseInt(document.getElementById("userinput").value);
+    attempts++;
+    if(usernum<1 || usernum>100){
+        alert("Enter a number between 1 and 100");
+    }
+    
+    else if(usernum === ans) {
+    document.getElementById("rbox").style.display="block"; 
+    document.getElementById("gboxcov").style.display="none"; 
+    document.getElementById("totalatt").innerHTML=`Total attempts: ${attempts}`;
+    document.getElementById("anspara").innerHTML=`You guessed the number Correctly (${ans}).`;
+    document.getElementById("btmimg").style.display="block";
+    document.getElementById("btmimg").style.animation="con 3s linear 1";
+    }
+    else if(usernum<ans){
+    document.getElementById("hilotxt").style.animation='none';
+   setTimeout(()=>{ document.getElementById("hilotxt").style.animation='pop 0.3s ease-out';
+ document.getElementById("hilotxt").innerText="Too Low, try again";  
+        document.getElementById("hilotxt").style.color="red";
+        document.getElementById("hilotxt").style.backgroundColor="#ff000a20";},50)
+        document.getElementById("guesscount").innerText=`No. Of Guesses: ${attempts}`;
+     document.getElementById("hilotxt").style.display = "block";
+    }
+    else{
+   document.getElementById("hilotxt").style.animation='none';
+    setTimeout(()=>{ document.getElementById("hilotxt").style.animation='pop 0.5s forwards';
+       
+       document.getElementById("hilotxt").innerText="Too High, try again";
+        document.getElementById("hilotxt").style.color="red";
+        document.getElementById("hilotxt").style.backgroundColor="#ff000a20";},50)
+        document.getElementById("guesscount").innerText=`No. Of Guesses: ${attempts}`;
+    }
+}
+
+//function to restart game
+function playagain() {
+   document.getElementById("hilotxt").innerText="...";
+    document.getElementById("hilotxt").style.color="black";
+    document.getElementById("guesscount").innerText=`No. Of Guesses:`;
+    document.getElementById("gboxcov").style.display="block"; 
+    document.getElementById("rbox").style.display="none"; 
+    document.getElementById("hilotxt").style.backgroundColor="white";
+    document.getElementById("userinput").value="";
+    document.getElementById("btmimg").style.display="none";
+    ans = genrandnum();
+    attempts = 0;
+    
+}
